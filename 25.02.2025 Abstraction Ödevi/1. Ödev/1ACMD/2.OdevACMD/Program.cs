6993b9cdd5c5bb9740e81712811fe511e0dc373f
Muhammed Hazar Çağlayan <hazarcglyn@gmail.com>
2025-03-04 13:56:25 +0300

    Add files via upload

---
+++ 25.02.2025 Abstraction Ödevi/1. Ödev/1ACMD/2.OdevACMD/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Reflection.Metadata;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BMW bMW = new BMW();
+        Mercedes mercedes = new Mercedes();
+        Porsche porsche = new Porsche();
+
+        Console.Write("BMW: ");
+        bMW.Speed();
+        bMW.Swim();
+        bMW.Fly();
+        Console.Write("Mercedes: ");
+        mercedes.Swim();
+        mercedes.Fly();
+        Console.Write("Porsche: ");
+        porsche.Fly();
+    }
+    interface ISpeed
+    {
+        public void Speed();
+    }
+    interface IFly
+    {
+        public void Fly();
+    }
+
+    interface ISwim
+    {
+        public void Swim();
+    }
+
+    class BMW : ISpeed, IFly, ISwim
+    {
+
+        public void Speed()
+        {
+            Console.Write("Çok Hızlı Gider. ");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Havada Uçar ");
+        }
+        public void Swim()
+        {
+            Console.Write("Denizde Yüzer ");
+        }
+    }
+
+    class Mercedes : IFly, ISwim
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Havada Uçar ");
+        }
+        public void Swim()
+        {
+            Console.Write("Denizde Yüzer ");
+        }
+    }
+
+    class Porsche : IFly
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Havada Uçar ");
+        }
+    }
+
+}
---
+++ 25.02.2025 Abstraction Ödevi/1. Ödev/InterFaceOdev.cs
@@ -0,0 +1,40 @@
+﻿interface ISpeed
+{
+    void Speed();
+}
+interface ISwim
+{
+    void Swim();
+}
+interface IFly
+{
+    void Fly();
+}
+class BMW : ISpeed, IFly, ISwim
+{
+    public void Speed()
+    {
+        Console.WriteLine("BMW: Çok hızlı gider, denizde yüzer, havada uçar.");
+    }
+}
+class Porsche : IFly
+{
+    public void Fly()
+    {
+        Console.WriteLine("Porsche: Havada uçar.");
+    }
+}
+class Mercedes : IFly, ISwim
+{
+    public void Swim()
+    {
+        Console.WriteLine("Mercedes: Denizde yüzer, havada uçar.");
+    }
+}
+Mercedes mercedes = new Mercedes();
+BMW bMW = new BMW();
+Porsche porsche = new Porsche();
+
+bMW.Speed();
+mercedes.Swim();
+porsche.Fly();
