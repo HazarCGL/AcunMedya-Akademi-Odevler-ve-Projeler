903895e48dd78fee394ab392d914a7f3f6e6fe4c
Muhammed Hazar Çağlayan <hazarcglyn@gmail.com>
2025-03-10 23:10:31 +0300

    Add files via upload

---
+++ 01.03.2025 Access Modifiers/01.03.2025 Access Modifiers/Program.cs
@@ -0,0 +1,61 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        User user = new User();
+        user.UserName = "Muhammed Hazar Caglayan";
+        user.UserAge = 19;
+        user.UserMail = "hazarcglyn@gmail.com";
+        user.BilgileriGoster();
+    }
+    // Public (Genel) Erişim Belirleyicisi
+    /*
+     * Bir Classdaki veriye Her yerden erişilebilir yapmaktadır.
+     */
+    // Private (Özel) Erişim Belirleyicisi
+    /*
+     * Bir Veriye Sadece Tanımlandığı Classdan Erişim izni verir.
+     */
+    //Protected (Korumalı) Erişim Belirleyicisi
+    /*
+     * Aynı Classda olan ve o classdan İnheritence alan sınıflar erişim sağlayabilir.
+     */
+    //internal (Dahili) Erişim Belirleyicisi
+    /*
+     * Aynı Proje içerisinden erişilebilir.
+     */
+    class User
+    {
+        private string adSoyad;
+        private int yas;
+        public string eMail; 
+        public string UserName
+        {
+            get { return adSoyad; }
+            set
+            {
+                adSoyad = value;
+            }
+        }
+        public int UserAge
+        {
+            get { return yas; }
+            set
+            {
+                yas = value;
+            }
+        }
+        public string UserMail
+        {
+            get { return eMail; }
+            set
+            {
+                eMail = value;
+            }
+        }
+        public void BilgileriGoster()
+        {
+            Console.WriteLine(adSoyad + "\n" + yas + "\n" + eMail);
+        }
+    }
+}
