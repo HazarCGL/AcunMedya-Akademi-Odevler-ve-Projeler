9eb9005c8cfd848880dbf7a896a65b336717e479
Muhammed Hazar Çağlayan <hazarcglyn@gmail.com>
2025-03-23 02:30:07 +0300

    Add files via upload

---
+++ 03.03.2025 Diziler ve Döngüler Ödevi/03.03.2025 Diziler ve Döngüler Ödevi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._03._2025_Diziler_ve_Döngüler_Ödevi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayi = new int[5];
+            Console.WriteLine("5 adet sayı giriniz: ");
+            for (int i = 0; i < 5; i++)
+            {
+                sayi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int toplam = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                toplam += sayi[i];
+            }
+            int ortalama = toplam / sayi.Length;
+            Console.WriteLine("Girdiğiniz sayilarin ortalaması: " + ortalama);
+        }
+    }
+}
