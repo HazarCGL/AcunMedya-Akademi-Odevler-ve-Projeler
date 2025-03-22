0697ab8f630926761ce5d082a478f8c3d48ad346
Muhammed Hazar Çağlayan <hazarcglyn@gmail.com>
2025-03-23 02:12:43 +0300

    Add files via upload

---
+++ 01.03.2025 Namespace Kullanımı/01.03.2025 Namespace Kullanımı/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+//Name Space Kullanmanın Avantajları
+//1.Aynı isime sahip sınıfları birbirinden ayırt etmek için kullanılır.
+//2.Kodun düzenli ve anlaşılır olmasını sağlar.
+//3.Aynı isimdeki sınıfları farklı namespace'lerde kullanabiliriz.
+//4.Aynı isime sahip sınıfları farklı namespace'lerde kullanabiliriz.
+//5.Proje genişletildiğinde yeni kodların eski kodlarla uyumlu olmasını kolaylaştırır.
+namespace _01._03._2025_Namespace_Kullanımı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hesaplama.Hesap hesap = new Hesaplama.Hesap();
+            Console.WriteLine(hesap.Topla(25, 56));
+            Console.WriteLine(hesap.carp(34, 26));
+            Console.ReadLine();
+        }
+    }
+}
+namespace Hesaplama
+{
+   public class Hesap
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public int carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+    }
+}
---
+++ 01.03.2025 Namespace Kullanımı/01.03.2025 Namespace Kullanımı/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("01.03.2025 Namespace Kullanımı")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01.03.2025 Namespace Kullanımı")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("24156fb5-b1b1-4a0d-b14b-6ab72079f79b")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
